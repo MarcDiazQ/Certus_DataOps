7a017f0b94609b4acb4d54f0ead79cfd0f9608c2
MarcDiazQ <u202021530@upc.edu.pe>
2024-04-02 20:57:16 +0000

    Inicio programa operaciones.py

---
+++ operaciones.py
@@ -0,0 +1,20 @@
+#Programa para el seguimiento de productos
+from datetime import datetime
+
+opc = 0
+while (opc < 1 or opc > 4):
+    print("Seleccione una opción:")
+    print("1. Venta de producto")
+    print("2. Contacto con cliente")
+    print("3. Asignar personal")
+    print("4. Producción de productos")
+    opc = int(input("Ingrese el número:"))
+
+if (opc == 1):
+  pass
+elif (opc == 2):
+  pass
+elif (opc == 3):
+  pass
+elif(opc == 4):
+  pass
